01004fd1f96c1c3499c4bae3fcec22dff215accd
Agnes Ching <agnesching19@gmail.com>
2018-03-06 19:15:29 +0000

    made an HTTP request

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=54%20Hitcham%20Road%20London&key=AIzaSyBzFoYFSBSry6W8sHO4g4WmKAq_qOCOVkA',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longtitude: ${body.results[0].geometry.location.lng}`);
 });
