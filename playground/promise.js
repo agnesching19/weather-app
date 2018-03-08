a53a8644cc2825f8accc0d638099f5f4eb70469f
Agnes Ching <agnesching19@gmail.com>
2018-03-08 22:21:31 +0000

    advanced promises

--- playground/promise.js
+++ playground/promise.js
@@ -1,12 +1,39 @@
-const somePromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    // resolve('Bello it works!');
-    reject('Unable to fulfill promise');
-  }, 2500);
+const asyncAdd = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === 'number' && typeof b === 'number') {
+        resolve(a + b);
+      } else {
+        reject('Arguments must be numbers');
+      }
+    }, 1500);
+  });
+};
+
+// const somePromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve('Bello it works!');
+//     reject('Unable to fulfill promise');
+//   }, 2500);
+// });
+
+// somePromise.then((message) => {
+//   console.log('Success: ', message);
+// }, (errorMessage) => {
+//   console.log('Error: ', errorMessage);
+// });
+
+asyncAdd(3, '5').then((result) => {
+  console.log('Result: ', result);
+  return asyncAdd(result, 23);
+}).then((result) => {
+  console.log('Should be 31', result);
+}).catch((errorMessage) => {
+  console.log(errorMessage);
 });
 
-somePromise.then((message) => {
-  console.log('Success: ', message);
+asyncAdd('hi', 17).then((result) => {
+  console.log('Result: ', result);
 }, (errorMessage) => {
-  console.log('Error: ', errorMessage);
+  console.log(errorMessage);
 });
